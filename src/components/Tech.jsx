608ad77c24a44d371448313583bb60f9f197d58d
Irfan shaik <irfan.shaik.4147@gmail.com>
2023-03-27 05:17:28 +0530

    first commit

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import  {BallCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { technologies } from '../constants';
