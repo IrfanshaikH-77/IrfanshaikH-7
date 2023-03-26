c2f28f30e7092325e98a5f1099ad22c2b8aeef17
Irfan shaik <irfan.shaik.4147@gmail.com>
2023-03-27 05:07:29 +0530

    first commit

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,9 +4,34 @@ import { SectionWrapper } from '../hoc';
 import { technologies } from '../constants';
 
 const Tech = () => {
+ 
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      { technologies.map((technology) => (
+     {!isMobile && technologies.map((technology) => (
        <div className='w-28 h-28 ' key={technology.name}>
         <BallCanvas
         icon={technology.icon}
@@ -17,4 +42,4 @@ const Tech = () => {
   )
 }
 
-export default  SectionWrapper(Tech, "" ) 
+export default  SectionWrapper(Tech, "" ) ;
